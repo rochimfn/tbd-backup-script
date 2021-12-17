188bca37bd1395653dff0d73520fdb6f85d9c6b7
Rochim Farul Noviyan <rochim.noviyan@gmail.com>
2021-12-17 22:53:12 +0700

    Fix relative path problem with .env

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
 import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const getConfig = () => {
-    dotenv.config()
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    dotenv.config({path:__dirname+'/.env'})
     const host = process.env.MONGO_HOST || '127.0.0.1';
     const username = process.env.MONGO_USERNAME || 'admin';
     const password = process.env.MONGO_PASSWORD || 'password';
